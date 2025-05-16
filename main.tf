2cf0c135c72c0c6e81b07d2c9bc5e7f29e9762ca
Sachin Kokare <70465292+skokare2312@users.noreply.github.com>
2025-05-16 11:16:58 +0530

    Update main.tf
    updated main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,23 @@ resource "tls_private_key" "keypair" {
   rsa_bits  = 4096
 
 }
+
+resource "aws_key_pair" "keypair" {
+
+  key_name   = "thinknyxkeypair"
+
+  public_key = tls_private_key.keypair.public_key_openssh
+
+  tags       = local.tags
+
+}
+
+resource "aws_instance" "aws_instance" {
+
+  ami                           = "ami-0e62a7626e25342b2"
+
+  instance_type                 = "t2.micro"
+
+  key_name                      = aws_key_pair.keypair.key_name
+
+}
